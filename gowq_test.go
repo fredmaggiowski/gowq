429add88806bd7053e04799ae7e5b0b37e9ee9fc
Federico Maggi <federico.maggi@mia-platform.eu>
2021-02-21 21:56:17 +0100

    test: items from loops must be copied

--- gowq_test.go
+++ gowq_test.go
@@ -56,3 +56,32 @@ func TestWithErrors(t *testing.T) {
 	require.Equal(t, 50, checkvalue, "Unexpected number of jobs executed")
 	require.Equal(t, 50, len(errors), "Some unexpected errors occurred")
 }
+
+func TestInRange(t *testing.T) {
+	wq := NewWQ(2)
+
+	checkvalue := 0
+	mtx := sync.Mutex{}
+
+	items := []struct {
+		val int
+	}{
+		{val: 10}, {val: 20}, {val: 30}, {val: 40}, {val: 50},
+		{val: 60}, {val: 70}, {val: 80}, {val: 90}, {val: 100},
+	}
+
+	for _, item := range items {
+		itemCopy := item
+		wq.Schedule(func(ctx context.Context) error {
+			mtx.Lock()
+			defer mtx.Unlock()
+
+			checkvalue += itemCopy.val
+			return nil
+		})
+	}
+
+	errors := wq.RunAll(context.TODO())
+	require.Equal(t, 100+90+80+70+60+50+40+30+20+10, checkvalue, "Unexpected number of jobs executed")
+	require.Equal(t, 0, len(errors), "Some unexpected errors occurred")
+}
