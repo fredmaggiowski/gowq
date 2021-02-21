71cf69e2edbe254613b3607509d43ed8ec19f14f
Federico Maggi <federico.maggi@mia-platform.eu>
2021-02-21 21:50:41 +0100

    add tests for error management

--- gowq_test.go
+++ gowq_test.go
@@ -2,6 +2,7 @@ package gowq
 
 import (
 	"context"
+	"fmt"
 	"sync"
 	"testing"
 
@@ -27,4 +28,31 @@ func Test(t *testing.T) {
 	errors := wq.RunAll(context.TODO())
 	require.Equal(t, 100, checkvalue, "Unexpected number of jobs executed")
 	require.Equal(t, 0, len(errors), "Some unexpected errors occurred")
+}
+
+func TestWithErrors(t *testing.T) {
+	wq := NewWQ(10)
+
+	checkvalue := 0
+	mtx := sync.Mutex{}
+
+	for i := 0; i < 100; i++ {
+
+		j := i
+		wq.Schedule(func(ctx context.Context) error {
+			if j%2 == 0 {
+				return fmt.Errorf("error %d", j)
+			}
+
+			mtx.Lock()
+			defer mtx.Unlock()
+
+			checkvalue++
+			return nil
+		})
+	}
+
+	errors := wq.RunAll(context.TODO())
+	require.Equal(t, 50, checkvalue, "Unexpected number of jobs executed")
+	require.Equal(t, 50, len(errors), "Some unexpected errors occurred")
 }
