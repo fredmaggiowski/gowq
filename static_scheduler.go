9e03972089b4f30d1bed8e28919e2c8529a05286
Federico Maggi <federico.maggi@mia-platform.eu>
2021-02-22 20:37:56 +0100

    refactor: splitted files

---
+++ static_scheduler.go
@@ -0,0 +1,44 @@
+package gowq
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+// RunAll runs all scheduled jobs and returns when all jobs terminated.
+func (w *WorkQueue) RunAll(ctx context.Context) []error {
+	workersQueue := make(chan bool, w.nWorkers)
+
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(len(w.staticJobQueue))
+
+	w.errorsList = make([]error, 0)
+
+	for i := 0; i < len(w.staticJobQueue); i++ {
+		job := w.staticJobQueue[i]
+
+		workersQueue <- true
+
+		go func(c context.Context, i int) {
+			if err := job(ctx); err != nil {
+				w.appendError(fmt.Errorf("%w [%d]: %s", ErrJobFailed, i, err.Error()))
+			}
+
+			waitGroup.Done()
+			_ = <-workersQueue
+		}(ctx, i)
+	}
+	waitGroup.Wait()
+	close(workersQueue)
+
+	return w.GetErrors(true)
+}
+
+// Push can be used to append a new job to the work queue.
+func (w *WorkQueue) Push(job Job) {
+	if w.staticJobQueue == nil {
+		w.staticJobQueue = make([]Job, 0)
+	}
+	w.staticJobQueue = append(w.staticJobQueue, job)
+}
